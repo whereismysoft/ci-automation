41bda6854e3c49901461429e736cb59d34f3c322
George <kotovyuriy27@gmail.com>
2021-11-01 16:38:57 +0300

    creating changelog

--- ci-source/entrypoint.sh
+++ ci-source/entrypoint.sh
@@ -1,5 +1,23 @@
 #!/bin/sh -l
 
-echo creating release $RELEASE_VERSION
+echo ----------------------------------------------------------------
+echo "\e[1m\e[35m release $RELEASE_VERSION"
+echo "\e[0m----------------------------------------------------------------"
+
+echo generating changelog file
+
+git tag -l --format='%(tag) %(contents)' > README.md
+
+git add README.md
+
+echo commiting change log file
+
+git commit -m "release changelog $RELEASE_VERSION"
+
+echo pushing changelog file to $RELEASE_VERSION
+
+git push origin $RELEASE_VERSION
 
 node ./ci-source/index.js
+
+# https://misc.flogisoft.com/bash/tip_colors_and_formatting
