b88934e8cc38ff2da6c4d0ef1d50e3ad985406ce
George <kotovyuriy27@gmail.com>
2021-11-02 16:07:39 +0300

    parsing ticket response

--- ci-source/entrypoint.sh
+++ ci-source/entrypoint.sh
@@ -1,25 +1,18 @@
-# #!/bin/sh -l
+#!/bin/sh -l
 
-# # скрипт должен создавать запись в реестре релизов (Трекер) 
-# # и сохранять туда всю нужную информацию: автор и дата релиза (можно взять из тэга), номер версии, changelog
+# скрипт должен создавать запись в реестре релизов (Трекер) 
+# и сохранять туда всю нужную информацию: автор и дата релиза (можно взять из тэга), номер версии, changelog
 
-# echo ----------------------------------------------------------------
-# echo "\e[1m\e[35m release $RELEASE_VERSION"
-# echo "\e[0m----------------------------------------------------------------"
+echo ----------------------------------------------------------------
+echo "\e[1m\e[35m release $RELEASE_VERSION"
+echo "\e[0m----------------------------------------------------------------"
 
-# sh ./ci-source/changelog-generator.sh
+sh ./ci-source/changelog-generator.sh
 
-# if [ $? -eq 1 ]; 
-#     then echo "\e[91m failed to generate CHANGELOG file \033[0m"; exit 1;
-# fi
+if [ $? -eq 1 ]; 
+    then echo "\e[91m failed to generate CHANGELOG file \033[0m"; exit 1;
+fi
 
-# # node ./ci-source/index.js
+# node ./ci-source/index.js
 
-# # formatting output - shttps://misc.flogisoft.com/bash/tip_colors_and_formatting
-
-RESPONSE=$(curl -X GET https://api.tracker.yandex.net/v2/issues/ | grep "login")
-
-echo $?
-
-echo $RESPONSE
-
+# formatting output - shttps://misc.flogisoft.com/bash/tip_colors_and_formatting
